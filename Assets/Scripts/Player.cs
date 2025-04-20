2828a6cb4a87de5d217dfc4328fe3a708b8d63d1
ResinDh <ferizi10rasim@gmail.com>
2025-04-21 00:17:59 +0200

    player movement and cctv

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    private float gravity = 1f;
+    public CharacterController crcon;
+    public Transform groundcheck;
+    public float distancetoground;
+    public LayerMask groundLayerMask;
+    Vector3 velocity;
+    bool isGrounded;
+    
+
+    
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundcheck.position, distancetoground, groundLayerMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        crcon.Move(move * speed * Time.deltaTime);
+        velocity.y -= gravity * Time.deltaTime;
+        crcon.Move(velocity);
+    }
+}
