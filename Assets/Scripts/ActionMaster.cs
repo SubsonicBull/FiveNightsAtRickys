601e9402bf60f81b12f36c1815dbd53a8621facb
SubsonicBull <97363116+SubsonicBull@users.noreply.github.com>
2025-04-22 00:08:31 +0200

    ActionMaster

---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class ActionMaster
+{
+    public static bool versteckt = false;
+    public static void Verstecken()
+    {
+        versteckt = !versteckt;
+    }
+}
