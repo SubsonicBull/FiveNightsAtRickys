d681a1fa7aad7b311c07c50a90a90745fac81a8d
SubsonicBull <97363116+SubsonicBull@users.noreply.github.com>
2025-04-22 19:39:08 +0200

    Gizmos

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -10,5 +10,14 @@ public class Waypoint : MonoBehaviour
     public bool IsOccupied() { return occupied; }
     public void Occupy() { occupied = true; }
     public void Free() { occupied = false; }
-    
+
+    private void OnDrawGizmos()
+    {
+        foreach(Waypoint w in neighbours)
+        {
+            Gizmos.DrawLine(transform.position, w.transform.position);
+            Gizmos.DrawSphere(transform.position + 0.75f * (w.transform.position - transform.position), 0.1f);
+            Gizmos.DrawCube(transform.position + 0.75f * (w.transform.position - transform.position) - (w.transform.position - transform.position).normalized * 0.1f, new Vector3(0.1f, 0.1f, 0.1f));
+        }
+    }
 }
