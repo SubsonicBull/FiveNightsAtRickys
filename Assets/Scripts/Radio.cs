72ad1d386965b4111ba8ce1e1883e6c8edc93f28
SubsonicBull <97363116+SubsonicBull@users.noreply.github.com>
2025-04-25 18:49:40 +0200

    Radio fertig

---
+++ Assets/Scripts/Radio.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections.Generic;
+using TMPro;
+
+public class Radio : Interactable
+{
+    [SerializeField] private List<AudioClip> songs;
+    [SerializeField] private TMP_Text displayText;
+    [SerializeField] private GameObject radioUI;
+    private int currentSongIndex = 0;
+    private AudioSource audioS;
+    void Start()
+    {
+        SetIsUIInteraction(true);
+        SetLocksPlayer(true);
+        SetUnlocksCursor(true);
+        SetName("Radio");
+        audioS = GetComponent<AudioSource>();
+        displayText.text = songs[currentSongIndex].name;
+        audioS.clip = songs[currentSongIndex];
+        audioS.Play();
+        radioUI.SetActive(false);
+    }
+
+    public override void UIInteract()
+    {
+        radioUI.SetActive(true);
+    }
+
+    public override void QuitUIInteraction()
+    {
+        radioUI.SetActive(false);
+    }
+    public void NextSong()
+    {
+        if (currentSongIndex < songs.Count - 1)
+        {
+            currentSongIndex++;
+            audioS.clip = songs[currentSongIndex];
+            audioS.Play();
+        }
+        else
+        {
+            currentSongIndex = 0;
+            audioS.clip = songs[currentSongIndex];
+            audioS.Play();
+        }
+        displayText.text = songs[currentSongIndex].name;
+    }
+}
